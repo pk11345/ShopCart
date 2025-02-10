082755c8254d799f9d2620596d369845f26ffb3e
pankaj kumar <117341395+pk11345@users.noreply.github.com>
2025-02-11 01:47:25 +0530

    first commit

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 // MongoDB Connection
-mongoose.connect("mongodb+srv://pk11345rai:4633pkrai@users.7iax2.mongodb.net/?retryWrites=true&w=majority&appName=users", { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("MongoDB Connected"))
   .catch(err => console.error(err));
 
